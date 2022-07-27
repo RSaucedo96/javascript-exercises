b16324a53be0f95949fb6b903789e127a91e7051
Victoria Gardella <ricardoarielsaucedo96@gmail.com>
2022-07-27 17:13:23 -0300

    finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(word,num) {
+    if (num <0){
+        return "ERROR";
+    }
+    else if (num==0){
+        return '';
+    }
+    else{
+        var Words="";
+        for (let i = 0; i < num; i++) {
+            Words = Words + word;
+        }
+    }
+    return Words;
 };
 
 // Do not edit below this line
