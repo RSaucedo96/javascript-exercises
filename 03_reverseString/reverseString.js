dc245a7f76d42fbb5df7759a1627846f7a7583e0
Ricardo <ricardoarielsaucedo96@gmail.com>
2022-07-28 15:34:42 -0300

    exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,21 @@
-const reverseString = function() {
+const reverseString = function(sentence) {
+    if (/\s/g.test(sentence) == true){
+        var words = sentence.split(' ').reverse();
+        var wordsCount;
+        var wordholder;
+        for(wordsCount = 0; wordsCount < words.length; wordsCount++) {
+            wordholder=words[wordsCount].split('').reverse();
+            words[wordsCount]=wordholder.join('')
+        }
+        return words.join(' ');
+    }
+    else if(sentence.length == 0){
+        return ('');
+    }
+    else{
+        var word=sentence.split('').reverse();
+        return word.join('');
+    }
 
 };
 
