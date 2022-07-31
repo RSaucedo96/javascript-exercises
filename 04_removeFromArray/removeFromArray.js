b3c1bc61b70cd858d1d408b63cd3be76ff0938e7
Ricardo <ricardoarielsaucedo96@gmail.com>
2022-07-31 16:03:11 -0300

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numbers,...args) {
+    var i = numbers.length;
+    while (i--) {
+        for (let j = 0; i < args.length; j++) {
+            if (numbers[i] === args[j]) {
+                numbers.splice(numbers.indexOf(args[j]), 1);
+            }
+        }    
+    }   
 };
 
 // Do not edit below this line
