2b99e7334421498d44dfe41665e7867366e25316
Victoria Gardella <ricardoarielsaucedo96@gmail.com>
2022-08-01 16:40:03 -0300

    completed exersice 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
-
+const sumAll = function(first ,last) {
+    if (first > last){
+        let sum=last;
+        while (last!=first){
+            last++;
+            sum+=last;    
+        }
+        return sum;
+    }
+    else if ((first || last)<1){
+        return 'ERROR'
+    }
+    else if(Number.isInteger(first) === false){
+        return 'ERROR'
+    }
+    else if(Number.isInteger(last) === false){
+        return 'ERROR'
+    }
+    else if(Array.isArray(first)){
+        return 'ERROR'
+    }
+    else if(Array.isArray(last)){
+        return 'ERROR'
+    }
+    let sum=first;
+    while (first!=last){
+        first++;
+        sum+=first;    
+    }
+    return sum;
 };
 
 // Do not edit below this line
