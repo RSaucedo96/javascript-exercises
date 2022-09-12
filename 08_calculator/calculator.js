f6021b9071bdd62c3c27960e8c0bf3650486197f
Ricardo <ricardoarielsaucedo96@gmail.com>
2022-09-12 13:03:03 -0300

    finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
-	
+const add = function(firstNumber,secondNumber) {
+	return firstNumber+secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+	return firstNumber-secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  var total=0;
+	if(numbers.isEmpty){
+    return 0;
+  }
+  else if(numbers.length == 1){
+    return numbers[0];
+  }
+  else{
+    numbers.forEach(element => {
+      total+=element;
+    });
+    return total;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  var total=1;
+  numbers.forEach(element => {
+    total*=element;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  var total=1;
+  for (let index = 0; index < power; index++) {
+    total*=number;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  var total=1;
+	if (number==0 || number==1){
+    return 1;
+  }
+  else{
+    for (let index = 1; index <= number; index++) {
+      total*=index;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
