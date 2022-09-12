54a17bd1e476598f7792310ca4095f61aa6cbd1a
Ricardo <ricardoarielsaucedo96@gmail.com>
2022-09-12 15:15:53 -0300

    finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,26 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    const noSpaces=sentence.replace(/\s/g, '').replace(regex, '');
+    sentence=sentence.toLowerCase().replace(regex, '');
+   
+    if (/\s/g.test(sentence) == true){
+        var words = sentence.split(' ').reverse();
+        var wordsCount;
+        var wordHolder;
+        for(wordsCount = 0; wordsCount < words.length; wordsCount++) {
+            wordHolder=words[wordsCount].split('').reverse();
+            words[wordsCount]=wordHolder.join('')
+        }
+        checker=words.join('').replace(/\s/g, '');
+    }
+    else{
+        const word=sentence.split('').reverse();
+        checker=word.join('').replace(/\s/g, '');
+    }   
+    if (checker===noSpaces) {
+        return true;
+    }
+    else {return false;}
 
 };
 
